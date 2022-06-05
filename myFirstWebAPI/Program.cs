5b52c7c8fd358dada80d63bb2086373da9cb7a16
upasna <upasnathakur726@gmail.com>
2022-06-05 23:20:46 +0200

    Initial commit for dotnet api BE

---
+++ myFirstWebAPI/Program.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using myFirstWebAPI.Data;
+
+namespace myFirstWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+
+            try{
+              context.Database.Migrate();
+              DbInitializer.Initialize(context);
+            }
+            catch(Exception ex){
+                logger.LogError(ex, "Migration Problem");
+            }
+            finally
+            {
+                scope.Dispose(); // dispose the resources
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
